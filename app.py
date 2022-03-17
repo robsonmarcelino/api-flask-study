8e89c7a049fa1078e797a268c4f2aec33783903a
Robson Marcelino <riggns@gmail.com>
2022-03-17 17:39:13 -0300

    make it better

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Hello World</h1>"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='localhost', port=port)
+def hello_world():
+    return "<p>Hello, World!</p>"
